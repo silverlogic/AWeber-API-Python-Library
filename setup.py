df4caa9b5043e6d39080e4a26fb8ee388920fb42
Jason Schadel <jasons@aweber.com>
2011-08-26 12:00:42 -0400

    Added setup and test dependencies to setup.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,16 @@ setup(
     version='1.1.0',
     packages=find_packages(exclude=['tests']),
     url='https://github.com/aweber/AWeber-API-Python-Library',
-    install_requires = ['oauth2 >= 1.2'],
+    install_requires = [
+        'oauth2 >= 1.2'
+        ],
+    tests_require = [
+        'dingus',
+        'coverage',
+        ],
+    setup_requires = [
+        'nose',
+        ],
     include_package_data=True
 )
 
